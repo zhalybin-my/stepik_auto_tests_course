ec9994cdb62ea57c26e55c096fdef217311c73d2
zhalybin-my <zhalybinmy@yandex.ru>
2022-06-30 15:50:27 +0300

    added my .py files

---
+++ 2.1path_step7.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+from selenium.webdriver.remote.webelement import WebElement
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    x_element = browser.find_element(By.ID, "treasure")
+    x = x_element.get_attribute("valuex")
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+    y = calc(x)
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+    option1 = browser.find_element(By.ID, "robotCheckbox")
+    option1.click()
+    option2 = browser.find_element(By.ID, "robotsRule")
+    option2.click()
+
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
